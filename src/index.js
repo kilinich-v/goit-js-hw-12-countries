44f147a998ce466ca2465bf9f6feb23100a48c5c
Vladimir Kilinich <kylynych.v@gmail.com>
2021-02-10 20:32:09 +0200

    правки в событиях
    - больше 10 стран не отображается
    - если строка поиска пустая, запрос не идет

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ refs.input.addEventListener('input', _.debounce(handlerCountries, 500));
 function handlerCountries(event) {
     const inputValue = event.target.value;
 
-    fetchCountries(inputValue).then(countries => {
-        if (countries.length > 10) {
-            creatAlerts(countries);
-
-            creatMarkup(countries);
-        } else {
-            creatMarkup(countries);
-        }
-    });
+    if (inputValue.length > 0) {
+        fetchCountries(inputValue).then(countries => {
+            if (countries.length > 10) {
+                creatAlerts(countries);
+            } else {
+                creatMarkup(countries);
+            }
+        });
+    }
 }
